ffda501e8598faf797e134c7c6b4713f744d370a
TomasPDA12 <160037379+tomasperezdelangel@users.noreply.github.com>
2025-11-06 16:47:38 -0600

    Correcciones en front/tutor

--- resources/views/modal/meses/mes3.blade.php
+++ resources/views/modal/meses/mes3.blade.php
@@ -32,10 +32,6 @@
                     </div>
                     <button type="submit" class="btn btn-primary">Guardar</button>
                 </form>
-
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
             </div>
         </div>
     </div>
